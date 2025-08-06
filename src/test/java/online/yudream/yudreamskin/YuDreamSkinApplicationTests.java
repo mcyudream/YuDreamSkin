3ae3521db2e9895d62f0c9b00c8deaef58e9c7e7
SiberianHusky <guojinlong@guojinlongdeMacBook-Air.local>
2025-08-07 00:22:44 +0800

    added: 初始化前端登陆

---
+++ src/test/java/online/yudream/yudreamskin/YuDreamSkinApplicationTests.java
@@ -0,0 +1,13 @@
+package online.yudream.yudreamskin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuDreamSkinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
